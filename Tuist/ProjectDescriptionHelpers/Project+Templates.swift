6a80772a5cef188f235d61d67d4047b3f9078825
Dora Lee <dsa28s@naver.com>
2023-07-12 01:04:52 +0900

    build: customize xcode settings property in makeModule function

--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -53,4 +53,30 @@ public extension Project {
             resourceSynthesizers: resourceSynthesizers
         )
     }
+
+    static func makeModule(name: String, targets: [Target], resourceSynthesizers: [ResourceSynthesizer], settings: Settings) -> Self {
+        let name: String = name
+        let organizationName: String? = nil
+        let options: Project.Options = .options()
+        let packages: [Package] = []
+        let settings: Settings = settings
+        let targets: [Target] = targets
+        let schemes: [Scheme] = []
+        let fileHeaderTemplate: FileHeaderTemplate? = nil
+        let additionalFiles: [FileElement] = []
+        let resourceSynthesizers: [ResourceSynthesizer] = resourceSynthesizers
+
+        return .init(
+            name: name,
+            organizationName: organizationName,
+            options: options,
+            packages: packages,
+            settings: settings,
+            targets: targets,
+            schemes: schemes,
+            fileHeaderTemplate: fileHeaderTemplate,
+            additionalFiles: additionalFiles,
+            resourceSynthesizers: resourceSynthesizers
+        )
+    }
 }
