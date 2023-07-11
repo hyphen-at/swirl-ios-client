6d25a9db98b8b8cd577e16e0f5c31d27114744d4
Dora Lee <dsa28s@naver.com>
2023-07-12 01:04:21 +0900

    dependencies: add hyphen sdk (local)

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -4,6 +4,7 @@ let SPM = SwiftPackageManagerDependencies([
     .remote(url: "https://github.com/pointfreeco/swift-composable-architecture", requirement: .upToNextMajor(from: "0.55.0")),
     .remote(url: "https://github.com/johnpatrickmorgan/TCACoordinators", requirement: .upToNextMajor(from: "0.4.0")),
     .remote(url: "https://github.com/davdroman/swiftui-navigation-transitions", requirement: .upToNextMajor(from: "0.10.1")),
+    .local(path: .relativeToRoot("hyphen-ios-sdk")),
 ])
 
 let dependencies = Dependencies(
