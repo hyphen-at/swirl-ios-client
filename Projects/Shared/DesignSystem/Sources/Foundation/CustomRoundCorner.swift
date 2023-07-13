cbd18b6ed6591eb6c2297f21e05184348386be2d
Dora Lee <dsa28s@naver.com>
2023-07-14 04:10:17 +0900

    feat(design): add CustomRoundedCorner modifier

---
+++ Projects/Shared/DesignSystem/Sources/Foundation/CustomRoundCorner.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+import UIKit
+
+public struct CustomRoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    public func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+public extension View {
+    func customCornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(CustomRoundedCorner(radius: radius, corners: corners))
+    }
+}
