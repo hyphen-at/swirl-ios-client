55e761cd941714b38e3ead664a1fcf7878f57cb3
Dora Lee <dsa28s@naver.com>
2023-07-15 03:59:37 +0900

    feat(design): add PHPickerViewController for SwiftUI

---
+++ Projects/Shared/DesignSystem/Sources/Representable/ImagePicker.swift
@@ -0,0 +1,44 @@
+import PhotosUI
+import SwiftUI
+
+public struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+
+    public init(image: Binding<UIImage?>) {
+        _image = image
+    }
+
+    public func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    public func updateUIViewController(_: PHPickerViewController, context _: Context) {}
+
+    public func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    public class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        let parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        public func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+
+            guard let provider = results.first?.itemProvider else { return }
+
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+        }
+    }
+}
